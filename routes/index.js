4928737c4cfe097be26fc21a2e2a17f3f3890ab3
Mersopolis <mersopolis@gmail.com>
2023-04-06 20:16:09 -0600

    Removed diagnostics.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ const express = require("express");
 
 // Import our modular routers for /notes and /diagnostics
 const notesRouter = require("./api/notes");
-const diagnosticsRouter = require("./api/diagnostics");
 
 const app = express();
 
 app.use("/notes", notesRouter);
-app.use("/diagnostics", diagnosticsRouter);
 
 module.exports = app;
